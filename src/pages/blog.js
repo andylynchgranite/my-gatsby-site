b83fe59f37e3fa8e0190b9c536bdd7f2506a8eb0
andylynchgranite <andrew.lynch@granite.ie>
2022-08-18 00:13:49 +0100

    Update To Blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,27 +4,32 @@ import Layout from '../components/layout'
 const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
   query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-        nodes {
-          name
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        excerpt
       }
+    }
   }
-`
+  `
 
 export default BlogPage
