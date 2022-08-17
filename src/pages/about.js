485ab7a84068dec27c92831ed6a3223170418c4e
andylynchgranite <andrew.lynch@granite.ie>
2022-08-17 01:05:22 +0100

    layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
     <main>
       <title>About Me</title>
+      <Link to="/">Home</Link>
       <h1>About Me</h1>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </main>
